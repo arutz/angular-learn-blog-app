d739016b77b61db04324e4879d7464e4c730e956
arutz <achim.rutz@googlemail.com>
2025-03-11 14:29:44 +0100

    Add About and Contact components with routing
    Replaced static about and contact HTML files with Angular components. Updated routes and navigation to include About and Contact links. Added styling and assets to enhance layout and user experience.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router'
 import { BlogComponent } from './blog/blog.component'
+import { AboutComponent } from './other/about/about.component'
+import { ContactComponent } from './other/contact/contact.component'
 
 export const routes: Routes = [
   { path: 'home', component: BlogComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ]
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-navigation',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core'
 })
 export class NavigationComponent {
 
+  router = inject(Router)
 }
